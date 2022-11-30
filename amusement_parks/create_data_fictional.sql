a9edeaea6356e680334fb8216c68dd6723fdfca6
Phineas Ziegler <phinziegler@gmail.com>
2022-11-30 16:41:25 -0500

    insert data

--- amusement_parks/create_data_fictional.sql
+++ amusement_parks/create_data_fictional.sql
@@ -2,6 +2,8 @@
 delete from park;
 delete from locaiton;
 delete from zone;
+delete from rating;
+delete from attractionzone;
 
 -- Parks:
 insert into park values ('001', 'CheeseLand', 'Cheese-themed park with rides and attractions themed after cheese and the cheesemaking process.  Fresh cheese available everywehre.', 'permanently closed');
@@ -26,3 +28,11 @@ insert into ridezone values('002', 'Curd Science Lab');
 
 -- MPA Film Ratings:
 -- example: insert into rating values ('G', 'General Audiences');
+
+-- Ratings
+insert into rating values('baby', 'Slow and safe, suitable for young children', 4);
+insert into rating values('intermediate', 'Fast, with quick turns, but no loops or extreme heights. Suitable for older children.', 10);
+insert into rating values('concussion', 'The fastest speeds and highest highs. Anything is fair game. Suitable for teenagers and adults.')
+
+-- Attraction Zones
+-- example: insert into attractionzone values('001', 'Cheddar Palace',)
