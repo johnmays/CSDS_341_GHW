d71279778d81d8356976a78c2706df7e6af5da69
mjk257 <mjk257@case.edu>
2022-11-30 21:32:49 -0500

    Added framework for use cases and divvied up

--- amusement_parks/amusement_parksDML.sql
+++ amusement_parks/amusement_parksDML.sql
@@ -1,19 +1,62 @@
--- find all parks in a country.
-CREATE PROCEDURE findAllParksInCountry @Country varchar(128)
-AS
-SELECT park.name FROM park, location WHERE country = @Country
-GO;
+-- Create Park - Tyler
 
--- find all rides in a specific park
-CREATE PROCEDURE findAllRidesInPark @Park varchar(128)
-AS
-SELECT * FROM ride WHERE park.name = @Park
-GO;
+-- Update Park - Jack
 
--- find all rides in a specific zone
-CREATE PROCEDURE findAllRidesInZone @Park varchar(3), @Zone varchar(128)
-AS
-SELECT ride.name FROM ridezone, ride, park WHERE park.name = @Park AND ridezone.zonename = @Zone
-GO;
+-- Create zone - Tyler
 
--- add ride to park
+-- Create ride - Phineas
+
+-- Update ride - Jack
+
+-- Delete ride - Phineas
+
+-- Select top speed ride at park - John
+
+-- Select zone at a park with most baby rides - John
+
+
+
+CREATE PROCEDURE Masterinsertupdatedelete (@id            INTEGER,
+                                          @first_name    VARCHAR(10),
+                                          @last_name     VARCHAR(10),
+                                          @salary        DECIMAL(10, 2),
+                                          @city          VARCHAR(20),
+                                          @StatementType NVARCHAR(20) = '')
+AS
+  BEGIN
+      IF @StatementType = 'Insert'
+        BEGIN
+            INSERT INTO employee
+                        (id,
+                         first_name,
+                         last_name,
+                         salary,
+                         city)
+            VALUES     ( @id,
+                         @first_name,
+                         @last_name,
+                         @salary,
+                         @city)
+        END
+
+      IF @StatementType = 'Select'
+        BEGIN
+            SELECT *
+            FROM   employee
+        END
+
+      IF @StatementType = 'Update'
+        BEGIN
+            UPDATE employee
+            SET    first_name = @first_name,
+                   last_name = @last_name,
+                   salary = @salary,
+                   city = @city
+            WHERE  id = @id
+        END
+      ELSE IF @StatementType = 'Delete'
+        BEGIN
+            DELETE FROM employee
+            WHERE  id = @id
+        END
+  END
