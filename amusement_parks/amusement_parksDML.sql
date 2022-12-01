e674652679fef18f966dcb66f873ccabffabb7a6
mjk257 <mjk257@case.edu>
2022-11-30 21:03:11 -0500

    Update amusement_parksDML.sql

--- amusement_parks/amusement_parksDML.sql
+++ amusement_parks/amusement_parksDML.sql
@@ -1,4 +1,24 @@
+<<<<<<< Updated upstream
 -- find all parks in the U.S.
 select park.name from park, location where country = 'USA' or country = 'U.S.A.' or country = 'United States'
+=======
+-- find all parks in a country.
+CREATE PROCEDURE findAllParksInCountry @Country varchar(128)
+AS
+SELECT park.name FROM park, location WHERE country = @Country
+GO;
+>>>>>>> Stashed changes
 
 -- find all rides in a specific park
+CREATE PROCEDURE findAllRidesInPark @Park varchar(128)
+AS
+SELECT * FROM ride WHERE park.name = @Park
+GO;
+
+-- find all rides in a specific zone
+CREATE PROCEDURE findAllRidesInZone @Park varchar(3), @Zone varchar(128)
+AS
+SELECT ride.name FROM ridezone, ride, park WHERE park.name = @Park AND ridezone.zonename = @Zone
+GO;
+
+-- add ride to park
