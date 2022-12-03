d6680a815ded43c20b370efa11069a4add0b851e
mjk257 <mjk257@case.edu>
2022-12-02 21:46:22 -0500

    Fixed syntax and removed example

--- amusement_parks/amusement_parksDML.sql
+++ amusement_parks/amusement_parksDML.sql
@@ -84,50 +84,5 @@ CREATE PROCEDURE selectZoneWithRating (@parkid int,
                                        @rating varchar(16))
   AS
     BEGIN
-      SELECT zonename, MAX(numrating) from (SELECT COUNT(*) AS numrating, ridezone.zonename as zonename from ride, riderating, ridezone WHERE ride.rideid = riderating.rideid AND ride.rideid = ridezone.rideid AND riderating.rating = @rating and ridezone.parkid = @parkid GROUP BY ridezone.zonename)
+     SELECT a.zonename, MAX(a.numrating) as maximum from (SELECT COUNT(*) AS numrating, ridezone.zonename as zonename from ride, riderating, ridezone WHERE ride.rideid = riderating.rideid AND ride.rideid = ridezone.rideid AND riderating.rating = @rating and ridezone.parkid = @parkid GROUP BY ridezone.zonename) a GROUP BY a.zonename
     END
-
-CREATE PROCEDURE Masterinsertupdatedelete (@id            INTEGER,
-                                          @first_name    VARCHAR(10),
-                                          @last_name     VARCHAR(10),
-                                          @salary        DECIMAL(10, 2),
-                                          @city          VARCHAR(20),
-                                          @StatementType NVARCHAR(20) = '')
-AS
-  BEGIN
-      IF @StatementType = 'Insert'
-        BEGIN
-            INSERT INTO employee
-                        (id,
-                         first_name,
-                         last_name,
-                         salary,
-                         city)
-            VALUES     ( @id,
-                         @first_name,
-                         @last_name,
-                         @salary,
-                         @city)
-        END
-
-      IF @StatementType = 'Select'
-        BEGIN
-            SELECT *
-            FROM   employee
-        END
-
-      IF @StatementType = 'Update'
-        BEGIN
-            UPDATE employee
-            SET    first_name = @first_name,
-                   last_name = @last_name,
-                   salary = @salary,
-                   city = @city
-            WHERE  id = @id
-        END
-      ELSE IF @StatementType = 'Delete'
-        BEGIN
-            DELETE FROM employee
-            WHERE  id = @id
-        END
-  END
